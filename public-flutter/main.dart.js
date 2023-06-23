c4328a3fd88bafd37c1306f020f8b2206762307f
Maximilien Mouton <maximouton@gmail.com>
2023-06-23 09:58:43 +0200

    test automatic deploy

--- public-flutter/main.dart.js
+++ public-flutter/main.dart.js
@@ -61906,7 +61906,7 @@ return!1},
 $S:25}
 A.kF.prototype={
 ao(){var s=null,r="Colonne 2",q=t.fQ
-return new A.Gk(A.a([A.Pt(A.a([A.h2(A.cM("Ligne 1",s),s,!1),A.h2(A.cM("Cell 1",s),s,!1),A.h2(A.cM("Cell 2",s),s,!1)],q)),A.Pt(A.a([A.h2(A.cM("Ligne 1",s),s,!1),A.h2(A.cM(" ",s),s,!1),A.h2(A.cM(" ",s),s,!1)],q)),A.Pt(A.a([A.h2(A.cM("Ligne 3",s),s,!1),A.h2(A.cM(r,s),s,!1),A.h2(A.cM(r,s),s,!1)],q))],t.xy),A.a([new A.kV(A.cM(" ",s),s,s),new A.kV(A.cM("Colonne 1",s),s,s),new A.kV(A.cM(r,s),s,s)],t.BR),B.j)}}
+return new A.Gk(A.a([A.Pt(A.a([A.h2(A.cM("Ligne 1",s),s,!1),A.h2(A.cM("Cell 1",s),s,!1),A.h2(A.cM("Cell 2",s),s,!1)],q)),A.Pt(A.a([A.h2(A.cM("Ligne 1",s),s,!1),A.h2(A.cM("Cell 3",s),s,!1),A.h2(A.cM("Cell 4",s),s,!1)],q)),A.Pt(A.a([A.h2(A.cM("Ligne 3",s),s,!1),A.h2(A.cM(r,s),s,!1),A.h2(A.cM(r,s),s,!1)],q))],t.xy),A.a([new A.kV(A.cM(" ",s),s,s),new A.kV(A.cM("Colonne 1",s),s,s),new A.kV(A.cM(r,s),s,s)],t.BR),B.j)}}
 A.Gk.prototype={
 L(a){var s=null,r=A.a9K(A.cM("Activit\xe9s",s)),q=this.e
 return A.ac0(r,new A.EX(B.at,new A.Aw(q,s,s,this.d,A.aiy(q),s),s),s,A.aax(A.aaK(B.yh,s),!1,new A.a_U(this)))}}
