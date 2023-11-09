f30ed2f976070bd705024c5cc0f773bdecc01203
Maximilien Mouton <maximouton@gmail.com>
2023-11-09 11:15:01 +0100

    introducing bloc

--- public-flutter/flutter_service_worker.js
+++ public-flutter/flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c5a751071e3b874597f2bbdc0f9fcfbd",
-"/": "c5a751071e3b874597f2bbdc0f9fcfbd",
-"main.dart.js": "1eb74b5df143eecdb8acf53ccde21440",
+"index.html": "8fbe7e7e86b734660135a8bd63991666",
+"/": "8fbe7e7e86b734660135a8bd63991666",
+"main.dart.js": "b9dbf4316a63c021188f7df21e70fdef",
 "manifest.json": "7380ba410d46ee30da4c0c63d2a57689",
 "version.json": "fa3fae63f489332d360aef8e171e1343"};
 // The application shell files that are downloaded before a service worker can
