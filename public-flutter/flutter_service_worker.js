39fc7836bd523cad55fb7af404137c8b03b8be68
Maximilien Mouton <maximouton@gmail.com>
2023-11-16 14:05:10 +0100

    essayer d'afficher l'id_token

--- public-flutter/flutter_service_worker.js
+++ public-flutter/flutter_service_worker.js
@@ -25,9 +25,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "7a56fa14f05cbf57568fff8cc84c74f7
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ed6556d20105e9d530ae462020573081",
-"/": "ed6556d20105e9d530ae462020573081",
-"main.dart.js": "e056c01b979ebb45617a3a243cd07e0d",
+"index.html": "83022d5e5d43f594784475c68093b328",
+"/": "83022d5e5d43f594784475c68093b328",
+"main.dart.js": "e6c7e7bfc7e92030b099249d69da589f",
 "manifest.json": "7380ba410d46ee30da4c0c63d2a57689",
 "version.json": "fa3fae63f489332d360aef8e171e1343"};
 // The application shell files that are downloaded before a service worker can
