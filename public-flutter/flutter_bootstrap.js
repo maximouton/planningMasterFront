9ea2496c6c70ab2f1a908da43c20f5aa68ff9f37
Maximilien Mouton <maximouton@gmail.com>
2024-06-19 20:11:16 +0200

    amélioration des indicateurs

--- public-flutter/flutter_bootstrap.js
+++ public-flutter/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3659052808"
+    serviceWorkerVersion: "2821432158"
   }
 });
